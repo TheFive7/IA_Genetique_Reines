fb03862c0b18095be43d06666b42c3551a4add66
Maxime <hennequin.maxime2412@gmail.com>
2022-12-01 00:10:33 +0100

    Correction de Mutation Correction de Couple Ajout de Evaluate Réagencement de tout le code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,21 +12,24 @@ using namespace std;
  * @param population : The Population
  * @param i : Indice of the person to display
  */
-void displayPopulation(int dimension, int* population[], int i) {
+void displayPopulation(int dimension, vector<vector<int>> population, int i) {
     for (int k = 0; k < dimension; k++) {
         cout << population[i][k] << ' ';
     }
     cout << endl;
 }
 
-/**
- * Initialize the population
- * @param dimension : Dimension of the person
- * @param population_size : Size of the maximum person in the population
- * @param population : The Population
- */
-void initPopulation(int dimension, int population_size, int* population[]) {
-    int person[dimension];
+int main() {
+    int nb_execution = 50;
+    int population_size = 50;
+    int dimension = 4;
+    int mutate_probability = 20;
+
+    vector<vector<int>> best_persons;
+    vector<vector<int>> population (population_size);
+
+    // Init random population
+    vector<int> person (dimension);
 
     for (int i = 0; i < population_size; i++) {
         // Create person with 1 number of each
@@ -35,93 +39,115 @@ void initPopulation(int dimension, int population_size, int* population[]) {
 
         // Shuffle
         for (int j = 0; j < dimension; j++) {
+            // TODO: random number not really random
             int indice = rand() % dimension;
             int tampon;
             tampon = person[indice];
             person[indice] = person[j];
             person[j] = tampon;
-
         }
         population[i] = person;
 
         // Display population
-        displayPopulation(dimension, population, i);
+        // displayPopulation(dimension, population, i);
     }
-}
 
-/**
- * Couple all person 2 by 2
- * @param dimension : Dimension of the person
- * @param population_size : Size of the maximum person in the population
- * @param population : The Population
- */
-void couplePopulation(int dimension, int population_size, int* population[]) {
-    for (int i = 0; i < population_size; i+=2) {
-        int *person1 = population[i];
-        int *person2 = population[i + 1];
+    // NB ITERATIONS
+    for (int n = 0; n < nb_execution; n++) {
+        // Couple
+        for (int i = 0; i < population_size; i+=2) {
+            vector<int> person1 = population[i];
+            vector<int> person2 = population[i + 1];
 
-        int moved = person1[i];
-        int crushed = person2[i];
+            int moved, crushed;
+            for (int j = 0; j < dimension; j++) {
+                moved = person1[j];
+                crushed = person2[j];
 
-        for (int j = 0; j < dimension; j++) {
-            if (person1[j] == crushed)
-                person1[j] = moved;
-            if (person2[j] == moved)
+                if (person1[j] == crushed)
+                    person1[j] = moved;
+
+                if (person2[j] == moved)
+                    person1[j] = crushed;
+
                 person1[j] = crushed;
+                person2[j] = moved;
+            }
+
+            population[i] = person1;
+            population[i + 1] = person2;
         }
 
-        person1[i] = crushed;
-        person2[i] = moved;
+        // Mutation
+        int indice, tampon;
+        for (int i = 0; i < population_size; i++) {
+            person = population[i];
+            if (rand() % 100 <= mutate_probability) {
+                indice = rand() % dimension;
 
-        population[i] = person1;
-        population[i + 1] = person2;
-    }
-}
+                tampon = person[indice];
+                person[indice] = person[person[indice]];
+                person[person[indice]] = tampon;
 
-/**
- * Mutate random person in the population
- * @param dimension : Dimension of the person
- * @param population_size : Size of the maximum person in the population
- * @param population : The Population
- * @param mutate_probability : Mutation probability (in percent)
- */
-void mutate(int dimension, int population_size, int* population[], int mutate_probability) {
-    int indice, tampon, replace_nb;
-    for (int i = 0; i < population_size; i++) {
-        int *person = population[i];
-        if (rand() % 100 <= mutate_probability) {
-            indice = rand() % dimension;
-            tampon = person[indice];
-            replace_nb = rand() % dimension;
-
-            for (int j = 0; j < dimension; j++) {
-                if (person[j] == replace_nb)
-                    person[j] = tampon;
+                population[i] = person;
             }
         }
-        person[indice] = replace_nb;
-        population[i] = person;
+
+        // Evaluate
+        int conflit;
+        for (int i = 0; i < population_size; i++) {
+            conflit = 0;
+            person = population[i];
+
+            for (int j = 0; j < dimension; j++) {
+                if (abs(j - (j + 1) % dimension) == abs(person[j] - person[(j + 1) % dimension])) {
+                    conflit++;
+                }
+            }
+
+            // If it has any conflict and the person is not in the array
+            if (conflit == 0) {
+                cout << "BEST: [ ";
+                for (int j = 0; j < dimension; j++) {
+                    cout << person[j] << ' ';
+                }
+                cout << ']' << endl;
+                exit(5);
+
+/*                // Add only if person is not yet in the array best_person
+                if (best_persons.empty()) {
+                    best_persons.push_back(person);
+                } else {
+                    bool same;
+                    for (auto & best_person : best_persons) {
+                        same = false;
+                        for (int k = 0; k < dimension; k++) {
+                            if (person[k] == best_person[k])
+                                same = true;
+                        }
+                        if (!same) {
+                            cout << "BEST: [ ";
+                            for (int j = 0; j < dimension; j++) {
+                                cout << person[j] << ' ';
+                            }
+                            cout << ']' << endl;
+
+                            best_persons.push_back(person);
+                        }
+                    }
+                }*/
+            }
+        }
+
+/*        // Print best persons
+        for (auto & best_person : best_persons) {
+            cout << "TAB: ";
+            for (int k = 0; k < dimension; k++) {
+                cout << best_person[k] << ' ';
+            }
+            cout << endl;
+        }
+
+        cout << best_persons.size() << endl;*/
     }
-}
-
-int main() {
-    int nb_execution = 50;
-    int population_size = 50;
-    int dimension = 4;
-    int mutate_probability = 20;
-
-    int* population[population_size];
-
-    // Init random population
-    initPopulation(dimension, population_size, population);
-
-
-    // Couple
-    couplePopulation(dimension, population_size, population);
-
-    // Mutation
-    mutate(dimension, population_size, population, mutate_probability);
-
-    // Test
-
 }
