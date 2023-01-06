ff9eadde2451dd656db82b6d429c60280a63563b
TheFive7 <hennequin.maxime2412@gmail.com>
2023-01-06 12:29:58 +0100

    Algorithme avec MPI Fini Commentaires

--- mpi/reine.cxx
+++ mpi/reine.cxx
@@ -1,3 +1,5 @@
+#include <mpi.h>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,6 +8,18 @@
 #include <ctime>
 
 using namespace std;
+
+// COMPIL & RUN
+// !mpicxx -o reine -fopenmp reine.cxx
+// !mpirun --allow-run-as-root -np 3 ./reine
+
+/**
+ * Project realised by HENNEQUIN Maxime
+ * December 2022 & January 2023
+ *
+ * Checkers algorithm with parallel processing
+ */
+
 
 // RANDOM
 int current_time_nanoseconds(){
@@ -121,6 +135,7 @@ vector<vector<int>> couple(int dimension, vector<vector<int>> population, int co
             population[i + 1] = person2;
         }
     }
+
     return population;
 }
 
@@ -175,51 +190,104 @@ vector<vector<int>> evaluate(int dimension, vector<int> person, vector<vector<in
         if (conflit == 0) {
             cout << "\n     BEST     : ";
             displayPerson(dimension, person);
+
+            // When a process find the best solution, it send a message to stop all process and exit the program
+            MPI_Abort(MPI_COMM_WORLD, nb_execution);
+            
             exit(nb_execution);
         }
     }
     return population;
 }
 
-int main() {
-    int nb_execution = 10000;
-    int population_size = 10;
+int main(int argc ,char *argv[]) {
+    int myrank, numprocs, tag = 100;
+    int send_at_iteration = 10;
+    int send_nb_person = 10; // Must be lower than population_size !
+
+    MPI_Status status;
+    MPI_Init (&argc ,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD ,&numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD ,&myrank);
+
+    int nb_execution = 100000;
+    int population_size = 30;
     int mutate_probability = 10;
     int couple_probability = 30;
-    int dimension = 5;
+    int dimension = 12;
 
-    vector<int> person (dimension);
-    vector<vector<int>> population (population_size);
+    vector<int> person (dimension); // Tab of a person
+    vector<vector<int>> population (population_size); // Tab of all person (the population)
 
     // Init random population
     population = initPopulation(dimension, person, population);
 
-    cout << "AU DEPART: ";
+    // Display the first person of the population
+    cout << "AT THE BEGINNING: ";
     displayPerson(dimension, population[0]);
 
-    // NB ITERATIONS
+    // Number of iterations
     for (int n = 0; n < nb_execution; n++) {
         // Couple
         population = couple(dimension, population, couple_probability);
 
-        cout << "APRES COUPLE  : ";
-        displayPerson(dimension, population[0]);
+        // cout << "AFTER COUPLE  : ";
+        // displayPerson(dimension, population[0]);
 
         // Mutation
         population = mutatePopulation(dimension, person, population, mutate_probability);
 
-        cout << "APRES MUTATION: ";
-        displayPerson(dimension, population[0]);
+        // cout << "AFTER MUTATION: ";
+        // displayPerson(dimension, population[0]);
 
         // Evaluate
         population = evaluate(dimension, person, population, n);
 
+        // cout << "AFTER EVAL    : ";
+        // displayPerson(dimension, population[0]);
+
         // Verify
-//        for (int i = 0; i < population_size; i++) {
-//            checkPerson(dimension, population[i]);
-//        }
+        // for (int i = 0; i < population_size; i++) {
+        //     checkPerson(dimension, population[i]);
+        // }
+
+        // SELECTION, MPI SEND AND RECV
+        if (n % send_at_iteration == 0) {
+            vector<vector<int>> selection (send_nb_person); // Tab of person to send
+            vector<vector<int>> conflit_at_indice (population.size()); // [[conflit, indice], ...]
+            int conflit;
+
+            // Calculate the conflict of each person
+            for (int i = 0; i < population.size(); i++) {
+                conflit = 0;
+                person = population[i];
+                for (int j = 0; j < dimension - 1; j++) {
+                    for (int k = j + 1; k < dimension; k++) {
+                        if (abs(j - k) == abs(person[j] - person[k])) {
+                            conflit ++;
+                        }
+                    }
+                }
+                conflit_at_indice[i] = { conflit, i };
+            }
+
+            // Sort the values to have the best in first
+            sort(conflit_at_indice.begin(),conflit_at_indice.end());
+
+            // Send person to next process (and receive from previous process)
+            for (int i = 0; i < send_nb_person; i++) {
+                selection[i] = population[conflit_at_indice[i][1]];
+
+                MPI_Send (&selection[i][0], dimension, MPI_INT, (myrank + 1) % numprocs, tag, MPI_COMM_WORLD);
+                MPI_Recv (&selection[i][0], dimension, MPI_INT, (myrank + (numprocs - 1)) % numprocs, tag, MPI_COMM_WORLD, &status);
+
+                population[conflit_at_indice[i][1]] = selection[i];
+            }
+        }
+
 
         // Shuffle the population
         shuffle(begin(population), end(population), rng);
     }
+    MPI_Finalize();
 }
